f1a3ecd014a517ed8672d8679bd2daf134a51609
intbeam <>
2021-01-16 17:38:37 +0100

    Added license. Changed filter to asterisk to not exclude C files

--- build.ps1
+++ build.ps1
@@ -1,3 +1,16 @@
+# Copyright 2021 intbeam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 param(
     [parameter(Mandatory = $false)][switch]$release = $false
 )
@@ -6,7 +19,7 @@ param(
 [string]$watcomInstallPath = "C:\WATCOM"
 
 # Retrieve all source code files (c, cpp) from src directory
-$files = (Get-ChildItem -Path "./src" -Filter "*.c??" -Recurse | Where-Object { $_.Extension.ToLowerInvariant() -in (".c", ".cpp")} | Select-Object -ExpandProperty FullName | ForEach-Object { """{0}""" -f $_ }  )
+$files = (Get-ChildItem -Path "./src" -Filter "*.c*" -Recurse | Where-Object { $_.Extension.ToLowerInvariant() -in (".c", ".cpp")} | Select-Object -ExpandProperty FullName | ForEach-Object { """{0}""" -f $_ }  )
 [string]$fileString = $files | Join-String -Separator " "
 
 # Check environment variables. If Environment contains watcom install path we will assume it has been set correctly
